659aa3d7704bfb15b466508786878401f06c77e1
khan168 <87687164+khan168@users.noreply.github.com>
2022-03-26 04:02:01 +0530

    models.py inside api folder added

---
+++ api/models.py
@@ -0,0 +1,30 @@
+
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+    class Meta:
+        ordering = ['-updated']
+
+
+#made chnage from here
+class profiles(models.Model):
+    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    name = models.CharField(max_length=80, default='name')
+    phone = models.CharField(max_length=80, default='813-xxx-xxxx')
+    address = models.CharField(max_length=80, default='abc street')
+    picUrl = models.CharField(max_length=200, default='xyz.jpeg')
+    workingHours = models.CharField(max_length=30, default='6-12am')
+    city = models.CharField(max_length=80, default='tampa')
+
+    def __str__(self):
+        return self.name
+
