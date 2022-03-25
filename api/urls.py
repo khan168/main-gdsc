25bd0692fe398e1ffd049fe15bd346081afead62
khan168 <87687164+khan168@users.noreply.github.com>
2022-03-26 04:03:13 +0530

    urls.py added inside api folder

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+#handles all url routing
+
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('',views.getRoutes),
+    path('notes/',views.getNotes),
+    path('notes/create/',views.createNote),
+    path('notes/<str:pk>/update/',views.updateNote),
+    path('notes/<str:pk>/delete/',views.deleteNote),
+    path('notes/<str:pk>/',views.getNote),
+    path('profile/create/', views.createprofile),
+    path('profile/<str:email>/', views.getprofile)
+
+    
+]
