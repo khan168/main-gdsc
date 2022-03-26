0c1a489f364acee4f4d0c46adc529c5648665b24
khan168 <umarahmedkhank6@gmail.com>
2022-03-26 02:42:30 -0400

    made change in admins.py file

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 # Register your models here.
-from .models import Note
+from .models import Note, profiles
 
 admin.site.register(Note)
+admin.site.register(profiles)
